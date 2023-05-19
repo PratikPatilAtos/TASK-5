058c07df25f33e2159785249ecaa5dae86182d19
PratikPatilAtos <133861933+PratikPatilAtos@users.noreply.github.com>
2023-05-19 17:48:49 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /*title = 'TASK05';*/
+
+  raw7sxr: string = ' '
+  raw21r7: string = ' '
+  raw54xq: string = ' '
+  raw1y8l: string = ' '
+  rawps2b: string = ' '
+  rawp7l1: string = ' '
+  rawtt0g: string = ' '
+  rawa1u5: string = ' '
+  rawwohk: string = ' '
+  raw89yg: string = ' '
+  rawox9c: string = ' '
+  rawf7aj: string = ' '
+  rawzfhe: string = ' '
+  rawvpy3: string = ' '
+  rawiy7r: string = ' '
+  rawm50n: string = ' '
+  constructor(private title: Title) {
+    this.title.setTitle('exported project')
+}
+}
